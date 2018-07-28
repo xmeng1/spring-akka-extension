ae0ed5a1da566c5eda89770a463a393a4875d14b
Xin Meng <x.meng@outlook.com>
2018-07-28 21:52:50 +0100

    only configuration the system actor and remove the annotation of configuration

--- src/main/java/science/mengxin/spring/akka/AkkaConfiguration.java
+++ src/main/java/science/mengxin/spring/akka/AkkaConfiguration.java
@@ -17,8 +17,8 @@ import org.springframework.context.annotation.DependsOn;
  * @author mengxin
  * @version 1.0
  */
-@Configuration
-@ComponentScan
+//@Configuration
+//@ComponentScan
 public class AkkaConfiguration {
 
     public static final String ACTOR_SYSTEM = "default-actorSystem";
@@ -34,17 +34,21 @@ public class AkkaConfiguration {
 
     private ActorSystem actorSystem;
 
+    public ActorSystem getActorSystem() {
+        return actorSystem;
+    }
+
     @Bean(name = ACTOR_SYSTEM)
     public ActorSystem actorSystem() {
         actorSystem = ActorSystem.create(ACTOR_SYSTEM);
         SPRING_EXTENSION_PROVIDER.get(actorSystem).initialize(applicationContext);
         return actorSystem;
     }
-
-    @Bean(name = WORK_ACTOR)
-    @DependsOn({ACTOR_SYSTEM})
-    public ActorRef workActor() {
-        return actorSystem
-                .actorOf(SPRING_EXTENSION_PROVIDER.get(actorSystem).props("workActor"), WORK_ACTOR);
-    }
+//
+//    @Bean(name = WORK_ACTOR)
+//    @DependsOn({ACTOR_SYSTEM})
+//    public ActorRef workActor() {
+//        return actorSystem
+//                .actorOf(SPRING_EXTENSION_PROVIDER.get(actorSystem).props("workActor"), WORK_ACTOR);
+//    }
 }
